eaa17b5165f0b2875006c3347611237a07995560
Lucas Ribeiro Gomes da Silva <101952246+LucaasRibeiro@users.noreply.github.com>
2022-09-27 19:59:53 -0300

    Add files via upload
    Atualização do Back End

--- Back end/Viagenslr/src/connection/ConnectionMySQL.java
+++ Back end/Viagenslr/src/connection/ConnectionMySQL.java
@@ -7,20 +7,9 @@ public class ConnectionMySQL {
 	public static Connection createConnectionMySQL() throws Exception{
 		
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		
-		
-		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/viagenslr?user=root&password=REcode7975@*");
-		
+		Connection connection = DriverManager
+				.getConnection("jdbc:mysql://localhost:3306/viagenslr?user=root&password=REcode7975@*");
 		return connection;
-		
 	}
-	public static void main(String[] args) throws Exception{
-		
-		Connection con = createConnectionMySQL();
-		
-		if (con != null) {
-			System.out.println(con + "\n****Conexão Bem Sucedidada****\n");
-			con.close();
-		}
-	}
+
 }
--- Back end/Viagenslr/src/dao/UsuarioDAO.java
+++ Back end/Viagenslr/src/dao/UsuarioDAO.java
@@ -7,162 +7,168 @@ import java.util.ArrayList;
 import java.util.List;
 
 import connection.ConnectionMySQL;
-import modelo.Usuarios;
-import modelo.Clientes;
-import modelo.Permissoes;
+import model.Usuario;
 
 public class UsuarioDAO {
-//CREATE
-	public void save(Usuarios usuario) {
-
-		String sql = "INSERT INTO usuarios " + "(login_usuario, senha_usuario, id_cliente, id_permissao)"
-				+ "VALUES (?,?,?,?,?)";
-
-		Connection conn = null;
-		PreparedStatement pstm = null;
-
-		try {
-			conn = ConnectionMySQL.createConnectionMySQL();
-			pstm = conn.prepareStatement(sql);
-
-			pstm.setString(1, usuario.getLogin_usuario());
-			pstm.setString(2, usuario.getSenha_usuario());
-
-			pstm.setString(3, usuario.getClientes().getId_cliente());
-			pstm.setString(4, usuario.getPermissoes().getId_permissao());
-
-			pstm.execute();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (pstm != null) {
-					pstm.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-
-	}
-
-	// DELETE BY ID
-	public void deleteById(int id) {
-		String sql = "DELETE FROM usuarios WHERE id_usuarios = ?";
-
-		Connection conn = null;
-		PreparedStatement pstm = null;
-
-		try {
-			conn = ConnectionMySQL.createConnectionMySQL();
-			pstm = conn.prepareStatement(sql);
-
-			pstm.setInt(1, id);
-
-			pstm.execute();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (pstm != null) {
-					pstm.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	// UPDATE
-	public void update(Usuarios usuario) {
-		String sql = "UPDATE usuarios " + "Set login_usuario = ?, senha_usuario = ?, id_cliente = ?, id_permissao = ?"
-				+ "WHERE id_usuario = ?";
-
-		Connection conn = null;
-		PreparedStatement pstm = null;
-
-		try {
-			conn = ConnectionMySQL.createConnectionMySQL();
-			pstm = conn.prepareStatement(sql);
-
-			pstm.setString(1, usuario.getLogin_usuario());
-			pstm.setString(2, usuario.getSenha_usuario());
-
-			pstm.setString(3, usuario.getClientes().getId_cliente());
-			pstm.setInt(4, usuario.getPermissoes().getId_permissao());
-
-			pstm.execute();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (pstm != null) {
-					pstm.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	// READ
-	public List<Usuarios> getUsuarios (){
-		String sql = "SELECT id_permissao FROM usuario_permissao;";
+	
+	//CREATE
+	public void save(Usuario pessoa) { 
 		
-		List<Usuarios> usuarios = new ArrayList<Usuarios>();
+		
+		String sql = "INSERT INTO usuario (nome_usuario) VALUES (?)";
+		
 		
 		Connection conn = null;
 		PreparedStatement pstm = null;
+		
+		try { 
+			conn = ConnectionMySQL.createConnectionMySQL();
+			
+			pstm = conn.prepareStatement(sql);
+			
+			pstm.setString(1, pessoa.getNome_usuario());
+			
+			pstm.execute();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally { 
+			
+			try { 
+				if (pstm != null) { 
+					pstm.close();
+				}
+				if (conn != null) { 
+					conn.close();
+				}
+			} catch (Exception e) { 
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	//READ
+	public List<Usuario> getUsuario() { 
+		
+		
+		String sql = "SELECT * FROM Usuario";
+		
+		
+		List<Usuario> pessoas = new ArrayList<Usuario>();
+		
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		
 		
 		ResultSet rset = null;
 		
 		try {
+			
+			conn = ConnectionMySQL.createConnectionMySQL();
+			
+			
+			pstm = conn.prepareStatement(sql);
+			
+			
+			rset = pstm.executeQuery();
+			
+			while (rset.next()) { 
+				Usuario pessoa = new Usuario();
+				
+				pessoa.setId_usuario(rset.getInt("id_usuario"));
+				pessoa.setNome_usuario(rset.getString("nome_usuario"));
+				
+				pessoas.add(pessoa);
+			}
+		} catch (Exception e) { 
+			e.printStackTrace();
+		} finally { 
+			try { 
+				if (rset != null) { 
+					rset.close();
+				}
+				if (pstm != null) { 
+					pstm.close();
+				}
+				if (conn != null) { 
+					conn.close();
+				}
+				
+			} catch (Exception e) { 
+				e.printStackTrace();
+			}
+		}
+		
+		return pessoas;
+	}
+
+	//UPDATE
+	public void update(Usuario pessoa) { 
+		
+		String sql = "UPDATE Usuario SET nome_usuario = ? WHERE id_usuario = ?";
+		
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		
+		try { 
 			conn = ConnectionMySQL.createConnectionMySQL();
 			
 			pstm = conn.prepareStatement(sql);
 			
-			rset = pstm.executeQuery();
 			
-			while (rset.next()) {
-				Usuarios usuario = new Usuarios ();
-				Permissoes permissao = new Permissoes ();
-				Clientes cliente = new Clientes ();
-				
-				usuario.setId_usuario(rset.getInt("id_usuario"));
-				usuario.setLogin_usuario(rset.getString("login_usuario"));
-				usuario.setSenha_usuario(rset.getString("senha_usuario"));
-				
-				permissao.setTipo_permissoes(rset.getString("tipo_permissao"));
-				usuario.setPermissoes;(permissao);
-				
-				usuarios.add(usuario);
-				
-				
-		} catch(Exception e) {
+			pstm.setString(1, pessoa.getNome_usuario());
+			pstm.setInt(2, pessoa.getId_usuario());
+			
+			pstm.execute();
+			
+		} catch (Exception e) { 
 			e.printStackTrace();
-		}finally {
-			try {
-				if (pstm != null) {
+		} finally { 
+			try { 
+				if (pstm != null) { 
 					pstm.close();
 				}
-				if (conn != null) {
+				if (conn != null) { 
 					conn.close();
 				}
-			}catch (Exception e) {
+				
+			} catch (Exception e) { 
 				e.printStackTrace();
 			}
 		}
-		return usuarios;
+	}
+	
+	//DELETE
+	public void deleteById(int id) { 
+		
+		String sql = "DELETE FROM Usuario WHERE id_usuario = ?";
+		
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		
+		try { 
+			conn = ConnectionMySQL.createConnectionMySQL();
+			
+			pstm = conn.prepareStatement(sql);
+			
+			pstm.setInt(1, id);
+			
+			pstm.execute();
+			
+		} catch (Exception e) { 
+			e.printStackTrace();
+		} finally { 
+			try { 
+				if (pstm != null) { 
+					pstm.close();
+				}
+				if (conn != null) { 
+					conn.close();
+				}
+				
+			} catch (Exception e) { 
+				e.printStackTrace();
+			}
+		}
 	}
 }
